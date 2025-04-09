c52debc01ea9150b2dad84313d42d32330ea0671
Shawn Kost <shawnmkost@gmail.com>
2025-04-09 11:59:59 -0700

    Add rate limit & cors/header config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,6 +23,28 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(50)))
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
+		MaxAge: 300,
+	}))
+
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{
+		XSSProtection:         "1; mode=block",
+		ContentTypeNosniff:    "nosniff",
+		XFrameOptions:         "DENY",
+		HSTSMaxAge:            31536000,
+		HSTSExcludeSubdomains: false,
+	}))
+
 	// Routes
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
